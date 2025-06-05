fbc004237824518db97a154cce255ea9aead447c
Meh Mol <m.mollah@proton.me>
2025-06-05 14:28:20 +0530

    Update 2.2.py

--- 2.2.py
+++ 2.2.py
@@ -3,16 +3,16 @@
 # prints the largest odd number among them. If none of them are odd, it should
 # print a message to that effect.
 
-a=int(input("Enter 1st integer "))
-b=int(input("Enter 2nd integer "))
-c=int(input("Enter 3rd integer "))
-print("You entered", a, b, c)   
+x=int(input("Enter 1st number "))
+y=int(input("Enter 2nd number "))
+z=int(input("Enter 3rd number "))
+print("You entered", x, y, x)   
 
-a=float('-inf') if a%2==0 else a
-b=float('-inf') if b%2==0 else b
-c=float('-inf') if c%2==0 else c
+x=float('-inf') if x%2==0 else x
+y=float('-inf') if y%2==0 else y
+z=float('-inf') if z%2==0 else z
 
-if a==float('-inf') and b==float('-inf') and c==float('-inf'):
-    print("There are no odd integers")
+if x==float('-inf') and y==float('-inf') and z==float('-inf'):
+    print("There are no odd number")
 else:
-    print("The largest odd integer is", max(a, b, c))
+    print("The largest odd number is", max(x, y, z))
