bce4f3557bd6482d8ad9672734b9dd3b86dd311a
Meh Mol <m.mollah@proton.me>
2025-06-05 14:26:15 +0530

    Update 2.2.py

--- 2.2.py
+++ 2.2.py
@@ -1,3 +1,8 @@
+# Pg. 18.
+# Finger exercise: Write a program that examines three variables—x, y, and z—and
+# prints the largest odd number among them. If none of them are odd, it should
+# print a message to that effect.
+
 a=int(input("Enter 1st integer "))
 b=int(input("Enter 2nd integer "))
 c=int(input("Enter 3rd integer "))
