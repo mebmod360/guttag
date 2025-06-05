ee1c6e4bf143e6b360b12b8c96cc2e11a069a663
Meh Mol <m.mollah@proton.me>
2025-06-05 19:02:32 +0530

    Update 3.1.py

--- 3.1.py
+++ 3.1.py
@@ -1,3 +1,16 @@
-x = int(input("Enter an integer"))
-root = 1
-pwr = 0
+x = int(input("Enter an integer "))
+root, root_check = 1, 1
+pwr, pwr_check = 1, 1
+while root ** pwr < x:
+    while pwr < 6 :
+        if root ** pwr == x:
+            print("Root is", root, "power is", pwr)
+            root_check = root
+            pwr_check = pwr
+            break
+        else:
+            pwr += 1
+    pwr = 1
+    root += 1
+if (root_check)**(pwr_check) != x :
+    print("No pair of integers exist")
