610f440e6827413bd140330e6b9abfb087f4eecd
Meh Mol <m.mollah@proton.me>
2025-06-05 15:29:32 +0530

    Update 2.4.py

--- 2.4.py
+++ 2.4.py
@@ -2,10 +2,10 @@
 # prints the largest odd number that was entered. If no odd number was entered, it
 # should print a message to that effect.
 
-x=float('-inf')
+x=float('-inf') #dummy variable whoch starts with lowest possible value
 
 for i in range(10):
     y=int(input("Enter an integer "))
-    if y%2==1 and y>x:
+    if y%2==1 and y>x: #checks if the entered value is odd and greater than the dummy variable
         x=y
 print("The largest odd integer is", x) if x != float('-inf') else print("There are no odd integers")
