2a5c8cec03490f904e563bdec755c8b9959dd4ae
Meh Mol <m.mollah@proton.me>
2025-06-05 15:30:34 +0530

    Update 2.4.py

--- 2.4.py
+++ 2.4.py
@@ -1,3 +1,4 @@
+# Pg. 24
 # Finger exercise: Write a program that asks the user to input 10 integers, and then
 # prints the largest odd number that was entered. If no odd number was entered, it
 # should print a message to that effect.
