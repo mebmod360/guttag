b1f922702925bed334fe9080923814743c621bf7
Meh Mol <m.mollah@proton.me>
2025-06-05 22:31:44 +0530

    Update 4.1.1.py

--- 4.1.1.py
+++ 4.1.1.py
@@ -1,12 +1,9 @@
-string1=input("Enter string 1 ")
-string2=input("Enter string 2 ")
-print(isIn(string1,string2))
-
-
 def isIn (a,b):
     if a in b or b in a:
         return True
     else: 
         return False
     
-
+string1=input("Enter string 1 ")
+string2=input("Enter string 2 ")
+print(isIn(string1,string2))
